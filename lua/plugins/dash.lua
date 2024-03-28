03eb02d5c0f5a0236088645d90d3e298c151864c
ntrx <kraenash@gmail.com>
2024-03-28 13:31:04 +0100

    add cringy customized dashboard with stupid art

---
+++ lua/plugins/dash.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "echasnovski/mini.starter",
+
+    opts = {
+      footer = "kaka",
+      header = [[
+        9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+         9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+          `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+            `9XXXXXXXXXXXP' `9XX'          `98v8P'          `XXP' `9XXXXXXXXXXXP'
+                ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                                )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                              ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                             dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                            dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                            9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                             `'      9XXXXXX(   )XXXXXXP      `'
+                                      XXXX X.`v'.X XXXX
+                                      XP^X'`b   d'`X^XX
+                                      X. 9  `   '  P )X
+      ]],
+    },
+  },
+}
